658dcaf37ceb4c920b4b2fddace6a0e6dc3251e8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-11-15 17:32:32 +0100

    dev: added header dom and css

--- front/src/components/layout/HeaderLayout.vue
+++ front/src/components/layout/HeaderLayout.vue
@@ -1,3 +1,40 @@
 <template>
-  <p>Header works!</p>
+  <header>
+    <a href="#">
+      <img src="/logo.svg" alt="Logo Gestion Stock" />
+      <span>Gestion Stock</span>
+    </a>
+  </header>
 </template>
+
+<style scoped>
+header {
+  background: #eee;
+  height: 3em;
+  display: flex;
+  align-items: center;
+  padding: 0 0.5em;
+}
+
+img {
+  height: 2.5em;
+}
+
+a {
+  color: inherit;
+  text-decoration: inherit;
+
+  display: flex;
+  align-items: center;
+  gap: 0.5em;
+}
+
+a span {
+  font-weight: bold;
+  font-size: 1.3em;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
