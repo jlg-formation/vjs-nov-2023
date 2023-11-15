4ec474c778d196942a3a5c3ac5199d5a1891635c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-11-15 12:04:41 +0100

    dev: added middleware to express

--- back/server.js
+++ back/server.js
@@ -1,2 +1,18 @@
 const express = require('express');
-console.log('express: ', express);
+
+const port = 3000;
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('req', req.url);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});
