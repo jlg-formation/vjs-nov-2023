9317721c65ff3e86c9eb91b8e9626d3f7a903e24
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-11-16 10:13:11 +0100

    dev: layout header + footer + sass

--- front/src/components/layout/FooterLayout.vue
+++ front/src/components/layout/FooterLayout.vue
@@ -1,3 +1,25 @@
 <template>
-  <p>Footer works!</p>
+  <footer>
+    <a href="#"> Mentions Légales </a>
+  </footer>
 </template>
+
+<style scoped lang="scss">
+footer {
+  background: #eee;
+  height: 3em;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  a {
+    color: inherit;
+    text-decoration: inherit;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
--- front/src/components/layout/HeaderLayout.vue
+++ front/src/components/layout/HeaderLayout.vue
@@ -7,34 +7,34 @@
   </header>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 header {
   background: #eee;
   height: 3em;
   display: flex;
   align-items: center;
   padding: 0 0.5em;
-}
 
-img {
-  height: 2.5em;
-}
+  a {
+    color: inherit;
+    text-decoration: inherit;
 
-a {
-  color: inherit;
-  text-decoration: inherit;
+    &:hover {
+      text-decoration: underline;
+    }
 
-  display: flex;
-  align-items: center;
-  gap: 0.5em;
-}
+    display: flex;
+    align-items: center;
+    gap: 0.5em;
 
-a span {
-  font-weight: bold;
-  font-size: 1.3em;
-}
+    img {
+      height: 2.5em;
+    }
 
-a:hover {
-  text-decoration: underline;
+    span {
+      font-weight: bold;
+      font-size: 1.3em;
+    }
+  }
 }
 </style>
