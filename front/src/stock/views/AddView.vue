e2efecc339b82c0a9fe7b6c3994556bddb8ebb3a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-11-16 17:08:51 +0100

    dev: useRouter

--- front/src/stock/views/AddView.vue
+++ front/src/stock/views/AddView.vue
@@ -1,7 +1,19 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const handleSubmit = (event: Event) => {
+  event.preventDefault()
+  console.log('coucou')
+  router.push({ name: 'stock' })
+}
+</script>
+
 <template>
   <main>
     <h1>Ajouter un article</h1>
-    <form>
+    <form @submit="handleSubmit">
       <label>
         <span>Nom</span>
         <input type="text" />
