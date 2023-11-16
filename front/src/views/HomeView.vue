070de78c82cd5b2c1fec861e26d92c30cc030b1e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-11-16 12:08:03 +0100

    dev: dom and css

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,3 +1,15 @@
 <template>
-  <p>Home works!</p>
+  <main>
+    <h1>Gérer efficacement votre stock !</h1>
+    <RouterLink to="/stock" class="button primary">
+      <span>Voir le stock</span>
+      <!-- fontawesome -->
+    </RouterLink>
+  </main>
 </template>
+
+<style scoped>
+main {
+  flex: 0;
+}
+</style>
--- front/src/views/LegalView.vue
+++ front/src/views/LegalView.vue
@@ -1,3 +1,25 @@
 <template>
-  <p>Legal works!</p>
+  <main>
+    <h1>Mentions Légales</h1>
+
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia ut deserunt consequuntur
+      accusamus a animi atque doloribus inventore cupiditate cum quidem dolores suscipit
+      necessitatibus mollitia magni, aliquam veritatis! Itaque, expedita.
+    </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis distinctio ea omnis quam
+      dolores vero, qui assumenda libero exercitationem obcaecati id deserunt fugit officiis! Quia
+      beatae repellendus distinctio omnis iure similique exercitationem asperiores ipsum architecto
+      esse dolore quae laborum, doloremque, maxime labore velit accusantium incidunt, dolorum iste
+      quidem ipsa officiis necessitatibus culpa odit. Laudantium, aperiam recusandae quod ex natus
+      dignissimos pariatur nemo at nam iusto? Optio, mollitia iusto reprehenderit nemo eum assumenda
+      ratione quibusdam delectus dicta aliquid repellendus vel modi eaque laudantium repudiandae
+      quisquam odio. Doloremque repellendus culpa rerum, voluptate aut cupiditate tenetur est
+      inventore deleniti possimus, consequuntur commodi odio.
+    </p>
+  </main>
 </template>
+
+<style scoped lang="scss"></style>
