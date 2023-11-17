cc092f46ac988bc8f9caa81f25b7850666a6a217
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-11-17 16:30:24 +0100

    dev: redirect for any other url

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -6,6 +6,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/:pathMatch(.*)*',
+      redirect: '/'
+    },
+    {
       path: '/',
       name: 'home',
       component: HomeView
