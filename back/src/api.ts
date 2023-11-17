a9b98539b9ccbf5b426f47b7ddfe51ee7c577023
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-11-17 13:57:11 +0100

    dev: added cors to server

--- back/src/api.ts
+++ back/src/api.ts
@@ -2,6 +2,11 @@ import express from 'express';
 
 const app = express.Router();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.get('/articles', (req, res) => {
   res.json([{ id: 'a1', name: 'Tournevis', price: 2.99, qty: 123 }]);
 });
