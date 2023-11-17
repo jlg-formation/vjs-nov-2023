ddae7e748acf4b878d1845b76bd631775cab4fc1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-11-17 11:21:54 +0100

    dev: added promisify

--- examples/async/promises.js
+++ examples/async/promises.js
@@ -1,30 +1,22 @@
 const fs = require("node:fs");
 
-const readdir = (...args) => {
-  return new Promise((resolve, reject) => {
-    // @ts-ignore
-    fs.readdir(...args, (err, result) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve(result);
+const promisify =
+  (asyncFn) =>
+  (...args) => {
+    return new Promise((resolve, reject) => {
+      // @ts-ignore
+      asyncFn(...args, (err, result) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(result);
+      });
     });
-  });
-};
+  };
 
-const readFile = (...args) => {
-  return new Promise((resolve, reject) => {
-    // @ts-ignore
-    fs.readFile(...args, (err, result) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve(result);
-    });
-  });
-};
+const readdir = promisify(fs.readdir);
+const readFile = promisify(fs.readFile);
 
 readdir(".")
   .then((files) => {
